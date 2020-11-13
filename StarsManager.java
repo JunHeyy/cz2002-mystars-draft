36dca2ff56fcc624787d3e6557f5903e9b717a29
JunHeyy <junjiexavier37@gmail.com>
2020-11-14 02:31:14 +0800

    new interface

---
+++ StarsManager.java
@@ -0,0 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface StarsManager {
+		private  void addIndex(Index newIndex) {
+	    	
+	    
+	    }
+
+
+	    private void removeIndex(int index) {
+    
+	        
+	    }
+	    
+	    
+	    private void extractIndex() {
+
+	    }
+	    
+	    private void  UpdateCourseDB(ArrayList<Index> indexList) {
+
+	    }
+	    
+
+}
