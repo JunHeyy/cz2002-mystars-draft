ab171f1a029d63d5f1b36dbb169bf0d2d1b5c806
d24ab <72213195+d24ab@users.noreply.github.com>
2020-11-18 02:59:03 +0800

    StaffController

--- StaffController.java
+++ StaffController.java
@@ -5,15 +5,17 @@ import java.util.ArrayList;
 public class StaffController{
 	
 
-	public void editStudentAccessPeriod(String matricNum, LocalTime startTime, LocalTime endTime) {
+	public static void editStudentAccessPeriod(String matricNum, LocalTime startTime, LocalTime endTime) { //tested
 		try {
 	        ArrayList<Student> studentList = StudentManager.extractDB();
 	        for (Student s: studentList) { 
-        		if(s.getMatricNum() == matricNum) {
+        		if(s.getMatricNum().equals(matricNum)) {
+        			System.out.println("Found student");
         			s.setStartAccessTime(startTime);
         			s.setEndAccessTime(endTime);
-        			System.out.println("Student " + s.getName() + "can only access the portal from "+
-        						startTime + "to" + endTime);  
+        			System.out.println("Student " + s.getName() + " can only access the portal from "+
+        						startTime + " to " + endTime);  
+        			StudentManager.UpdateStudentDB(studentList);
         			break;
         		
         		}
@@ -25,8 +27,24 @@ public class StaffController{
 			
 		}
 	}
-	
-	public static void addStudent(Student student) {
+	public static void printSelectedStudent(String matricsNum) throws ClassNotFoundException, IOException {
+		ArrayList<Student> studentList = StudentManager.extractDB();
+		for(Student s: studentList){
+			if(s.getMatricNum().equals(matricsNum)) {
+				System.out.printf("Student name: " + s.getName() +" Matrics Number: " + s.getMatricNum() +  " Gender: " + 
+						 " Nationality: " + s.getNationality() + " Year of Study: " + s.getYearOfStudy()+  " Access time from " + s.getStartAccessTime() + " to " + s.getEndAccessTime() +"\n");
+				StudentController.printCourseRegistered(matricsNum);
+			}
+		}
+		
+	}
+	public static void addStudent(String username, String password, String name,  //tested
+    		String matricNum, char gender, String nationality, 
+    		int yearOfStudy, LocalTime startAccessTime,
+    		LocalTime endAccessTime) {
+		ArrayList<Index> registeredIndex = new ArrayList<Index>();
+		Student student = new Student ( username,  password,  name, matricNum,  gender,  nationality, 
+										yearOfStudy,  registeredIndex,  startAccessTime, endAccessTime);
 		try {
 			StudentManager.addStudent(student);
 		} catch (ClassNotFoundException | IOException e) {
@@ -38,16 +56,16 @@ public class StaffController{
 		}
 	}
 	
-	public void addCourse(Course newCourse) {
+	
+	
+	public static void addCourse(String courseCode, String courseName, int numAUs, ArrayList<Index> indexList) { //Tested
+		Course newCourse = new Course(courseCode, courseName, numAUs,  indexList);
 		try {
 			CourseManager.addCourse(newCourse);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+		} catch (ClassNotFoundException e) {		
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			e.printStackTrace();		}
 		catch(Exception e) {
 			System.out.println("There was an error");
 		}
@@ -58,7 +76,10 @@ public class StaffController{
 
 	}
 	
-	public static void printStudentByIndex(int IndexNum) {
+	public void addIndex(String courseCode, Index newIndex) {
+		//TODO
+	}
+	public static void printStudentByIndex(int IndexNum) { //tested
 		//Need to test!!
 		try {
 	        ArrayList<Student> studentList = StudentManager.extractDB();
@@ -66,7 +87,7 @@ public class StaffController{
 	        	ArrayList<Index> registeredIndex;
 	        	//Get the indexArray the student object register
 	        	registeredIndex = s.getRegisteredIndex();
-	        	if(registeredIndex ==null) System.out.println("There are no students registered");
+	        	if(registeredIndex == null) System.out.println("There are no students registered");
 	        	for(Index index : registeredIndex) {
 	        		if(index.getIndexNum() == IndexNum) {
 	        			System.out.println("Student " + s.getName() + " is registered in "+
@@ -83,13 +104,13 @@ public class StaffController{
 		}
 	}
 	
-	public static void printStudentByCourse(String courseCode) {
+	public static void printStudentByCourse(String courseCode) { //Tested
 		try {
 	        ArrayList<Student> studentList = StudentManager.extractDB();
 	        
 	        for (Student s: studentList) { 
-	        	ArrayList<Index> registeredIndex;
-	        	registeredIndex = s.getRegisteredIndex();
+	        	ArrayList<Index> registeredIndex= s.getRegisteredIndex();
+	        	
 	        	for(Index index : registeredIndex) {
 	        		if(index.getCourseCode().equals(courseCode)) {
 	        			System.out.println("Student " + s.getName() + " is registered in "+
@@ -105,7 +126,7 @@ public class StaffController{
 		}
 	}
 	
-	public static void printAllCourses() {
+	public static void printAllCourses() { //Tested
 		try {
 	        ArrayList<Course> courseList = CourseManager.extractDB();
 	        System.out.println("These are the following Courses registered in the course database");
