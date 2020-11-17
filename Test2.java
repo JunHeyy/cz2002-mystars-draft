bd70c1c6cfaec4b59dc237c3947c088dd7b4a106
d24ab <72213195+d24ab@users.noreply.github.com>
2020-11-18 06:37:29 +0800

    latest

--- Test2.java
+++ Test2.java
@@ -16,22 +16,27 @@ public class Test2 {
 		//StudentManager.addStudent(s2);
 		
 		StudentController SC = new StudentController();
+		StaffController SF = new StaffController();
+		
+		//SF.printSelectedStudent("U199999");
+		//SF.editStudentAccessPeriod("U199999", LocalTime.of(12,43,12), LocalTime.of(14,43,12));
+		//SF.printSelectedStudent("U199999");
         
 		//StudentController.addCourse("U192456", 2);
 		//StudentController.addCourse("U199999", 1);
         //SC.addCourse(s1, 1);
 		
 
-		StudentController.printCourseRegistered("U192456");
-		StudentController.printCourseRegistered("U199999");
+		//StudentController.printCourseRegistered("U192456");
+		//StudentController.printCourseRegistered("U199999");
 		
 		//StudentController.addCourse("U199999", 4);
 		//StudentController.removeCourse("U192456", "CZ2002");
 		//StudentController.changeIndex("U192456", 5, 4, "CZ2005");
 		//StudentController.swapIndex("U192456", 5, 4, "U199999", "123");
 		
-		StudentController.printCourseRegistered("U192456");
-		StudentController.printCourseRegistered("U199999");
+		//StudentController.printCourseRegistered("U192456");
+		//StudentController.printCourseRegistered("U199999");
         
         
 	}
